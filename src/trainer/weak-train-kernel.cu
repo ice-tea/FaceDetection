6877896a43ee5cdf0dc377ce9d7d4679195d0f70
ice-tea <tmacback@gmail.com>
2017-05-03 13:49:48 -0400

    comment cpu

--- src/trainer/weak-train-kernel.cu
+++ src/trainer/weak-train-kernel.cu
@@ -9,9 +9,7 @@ __constant__ bool V[TNUM];
 __constant__ double W[TNUM];
 
 __global__ void KernelWeakTrain(int featureNum, int testNum, int *tindex, 
-    double validweight, int* indexR, bool* goodR, double* errorR
-    /*, bool * V, double * W*/) {
-    // Get our global thread ID
+    double validweight, int* indexR, bool* goodR, double* errorR ) {
     int id = blockIdx.x*blockDim.x+threadIdx.x;
 
     if(id >= FNUM)
@@ -38,7 +36,6 @@ __global__ void KernelWeakTrain(int featureNum, int testNum, int *tindex,
                 local_best = positive_error;
                 loca_good = true;
                 local_index = i;
-              //best = TestWeakClassifier(feature, feature.values_[itest].value_ + 1, 1, positive_error);
             }
         }
         else{
@@ -46,13 +43,9 @@ __global__ void KernelWeakTrain(int featureNum, int testNum, int *tindex,
             negative_error = 1.0 - positive_error;
 
             if (negative_error < local_best){
-                //errorR[id] = negative_error;
-                //goodR[id] = false;
-                //indexR[id] = i;
                 local_best = negative_error;
                 loca_good = false;
                 local_index = i;
-              //best = TestWeakClassifier(feature, feature.values_[itest].value_ - 1, -1, negative_error);
             }
         }
         pos++;
@@ -65,24 +58,16 @@ __global__ void KernelWeakTrain(int featureNum, int testNum, int *tindex,
 void select_best_gpu(int featureNum, int testNum, bool * valids, double * weights, double validweight, int* featureIndex,
     int * indexResult, bool * goodResult, double * errorResult){
 
+    //Constant Memory
     cudaMemcpyToSymbol(V, valids, testNum *sizeof(bool));
     cudaMemcpyToSymbol(W, weights, testNum *sizeof(double));
 
+    //Global For each Feature
     int * d_f_i;
     cudaMalloc(&d_f_i, featureNum  * testNum * sizeof( int ));
     cudaMemcpy(d_f_i, featureIndex, featureNum  * testNum * sizeof( int ), cudaMemcpyHostToDevice);
 
-    //constant
-    /*
-    bool * V;
-    cudaMalloc(&V, testNum *sizeof(bool));
-    cudaMemcpy(V, valids, testNum *sizeof(bool), cudaMemcpyHostToDevice);
-    double * W;
-    cudaMalloc(&W, testNum *sizeof(double));
-    cudaMemcpy(W, weights, testNum *sizeof(double), cudaMemcpyHostToDevice);
-    */
-
-    // Launch the device computation threads!
+    // Global For Result
     int * d_i;
     bool * d_g;
     double * d_e;
@@ -98,8 +83,6 @@ void select_best_gpu(int featureNum, int testNum, bool * valids, double * weight
     cudaMemcpy(errorResult, d_e, featureNum *sizeof(double), cudaMemcpyDeviceToHost);
 
     // Free device matrices
-    //cudaFree(V);
-    //cudaFree(W);
     cudaFree(d_f_i);
     cudaFree(d_i);
     cudaFree(d_g);
